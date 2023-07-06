4de3e36a2b20809048e2c7781e020db381bce818
eya26 <eya.chaftar26@gmail.com>
2023-07-07 01:49:23 +0200

    Implementing game routes

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
+const GameRoutes = require('./routes/game');
+
 const App = express();
 App.use(helmet());
 App.use(morgan('common'));
+App.use(express.json());
 
 App.get('/',(request, response) => {
     return response.status(200).send('<h1>It works !</h1>');
@@ -14,8 +17,10 @@ App.get('/',(request, response) => {
 
 App.post('/',(request,response) => {
     return response.status(200).send('<h1>It works !</h1>');
-})
+});
 
+
+App.use('/game', GameRoutes);
 const PORT = 5000
 App.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
